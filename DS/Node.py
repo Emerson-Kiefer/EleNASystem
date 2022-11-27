ba31457242bae6d357f7813cc6ae13d5133877f8
Emerson-Kiefer <emerson.kiefer01@gmail.com>
2022-11-27 00:51:03 -0500

    Added list of neighbors to node object

--- DS/Node.py
+++ DS/Node.py
@@ -2,11 +2,13 @@ class Node:
     # private String nodeName;
     # private Double longitude;
     # private Double latitude;
+    # private List neighbors;
 
-    def __init__(self, name, longi, lati):
+    def __init__(self, name, longi, lati, neighbors):
         self._nodeName = name
         self._longitude = longi
         self._latitude = lati
+        self._neighborList = neighbors
 
     def getNodeName(self):
         return self._nodeName
@@ -16,6 +18,10 @@ class Node:
 
     def getLatitude(self):
         return self._latitude
+    
+    def getNeighbors(self):
+        return self._neighborList
 
     def __repr__(self):
         return "<nodeName = {}, longitude = {}, latitude = {}>".format(self._nodeName, self._longitude, self._latitude)
+    
