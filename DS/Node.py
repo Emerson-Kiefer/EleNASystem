d3696d4379de5cc415fda0cae923ca2548649198
Emerson-Kiefer <emerson.kiefer01@gmail.com>
2022-11-28 04:08:30 -0500

    Changed name attribute to id for node

--- DS/Node.py
+++ DS/Node.py
@@ -1,17 +1,28 @@
 class Node:
-    # private String nodeName;
-    # private Double longitude;
-    # private Double latitude;
-    # private List neighbors;
+    '''
+        int id:
+            the id of the node (name of the intersection)
+        Double longitude;
+            the longitude of the node (from OpenStreetMap)
+        Double latitude;
+            the latitude of the node (from openStreetMap)
+        Double elevation
+            the elevation of the node (from an elevation API)
+        Dict neighbors;
+            A dictionary where keys are the names of neighboring nodes, 
+            and each value is a dictionary containing the "Distance" 
+            traveled and "Elevation" gain along that edge
+    '''
 
-    def __init__(self, name, longi, lati, neighbors):
-        self._nodeName = name
+    def __init__(self, id, longi, lati, elevation, neighbors):
+        self._id = id
         self._longitude = longi
         self._latitude = lati
+        self._elevation = elevation
         self._neighborList = neighbors
 
-    def getNodeName(self):
-        return self._nodeName
+    def getId(self):
+        return self._id
 
     def getLongitude(self):
         return self._longitude
@@ -19,9 +30,12 @@ class Node:
     def getLatitude(self):
         return self._latitude
     
+    def getElevation(self):
+        return self._elevation
+    
     def getNeighbors(self):
         return self._neighborList
 
     def __repr__(self):
-        return "<nodeName = {}, longitude = {}, latitude = {}>".format(self._nodeName, self._longitude, self._latitude, self._neighborList)
+        return "<id = {}, longitude = {}, latitude = {}, elevation = {}, neighborList = {}>".format(self._id, self._longitude, self._latitude, self._elevation, self._neighborList)
     
