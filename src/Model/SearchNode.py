d3b1f514eb03ae4ef618f6f93b57291caea66de4
Emerson-Kiefer <emerson.kiefer01@gmail.com>
2022-11-30 13:27:37 -0500

    Created search node for a_star algorithm: contains node, minimum cost, value from heuristic, and parent node

---
+++ src/Model/SearchNode.py
@@ -0,0 +1,51 @@
+class SearchNode:
+    '''
+        Node node:
+            a node from the Node class
+        Double minCost:
+            The minimum cost to reach this node from the start node
+        Double heuristicValue:
+            The value of the heuristic's estimate for the cost from this node to the goal node
+    '''
+
+    def __init__(self, node, minCost, heuristicValue, parentNode):
+        self._node = node
+        self._minCost = minCost
+        self._heuristicValue = heuristicValue
+        self._parentNode = parentNode
+
+    def getNode(self):
+        return self._node
+
+    def getMinCost(self):
+        return self._minCost
+
+    def getHeuristicValue(self):
+        return self._heuristicValue
+
+    def getPriority(self):
+        return self._minCost + self._heuristicValue
+
+    def getParentNode(self):
+        return self._parentNode
+    
+    '''
+        Compare the cost of a new path from start --> current with the current minimum cost. 
+        Update the minCost and parentNode if the new cost is shorter and return True
+            return False if no update was required
+    '''
+    def updateMinDistanceTraveled(self, newCost, newParentNode):
+        if newCost > self._minCost:
+            return False
+        self._minCost = newCost
+        self._parentNode = newParentNode
+        return True
+
+
+    def setHeuristicValue(self, newHeuristicValue):
+        self._heuristicValue = newHeuristicValue
+
+
+    def __repr__(self):
+        return "<node = {}, minCost = {}, heuristicValue = {}, parentNode = {}>".format(self._node, self._minCost, self._heuristicValue, self._parentNode)
+    
