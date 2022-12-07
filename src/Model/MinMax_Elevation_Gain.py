e41569386f20d201436c4b820e9425029aeebc10
Emerson-Kiefer <emerson.kiefer01@gmail.com>
2022-12-07 14:34:17 -0500

    Removed commented obsolete code

--- src/Model/MinMax_Elevation_Gain.py
+++ src/Model/MinMax_Elevation_Gain.py
@@ -6,11 +6,11 @@ from SearchNode import SearchNode
 import math
 
 S = Node("S", 24, 49, 0, {})
-A = Node("A", 24.10, 49.10, 100, {})
-B = Node("B", 23.99, 49.01, 1000, {})
-C = Node("C", 24.00, 49.01, 20, {})
-D = Node("D", 48, 48, 10, {})
-G = Node("G", 24, 49.3, 80, {})
+A = Node("A", 24.10, 49.10, -100, {})
+B = Node("B", 23.99, 49.01, 10, {})
+C = Node("C", 24.00, 49.01, 10, {})
+D = Node("D", 48, 48, 11, {})
+G = Node("G", 24, 49.3, 10, {})
 
 S.addNeighbor(A, S.getHaversineDistance(A), S.getElevationGain(A))
 S.addNeighbor(B, S.getHaversineDistance(B), S.getElevationGain(B))
@@ -26,45 +26,11 @@ D.addNeighbor(B, D.getHaversineDistance(B), D.getElevationGain(B))
 D.addNeighbor(G, D.getHaversineDistance(G), D.getElevationGain(G))
 G.addNeighbor(A, G.getHaversineDistance(A), G.getElevationGain(A))
 G.addNeighbor(D, G.getHaversineDistance(D), G.getElevationGain(D))
-
-# def min_elevation(startNode, goalNode, optimalPathLength, percentOfOptimal):
-#     maxPathLength = optimalPathLength * (1+percentOfOptimal)
-
-#     ''' fronteirQ is a priority queue containing tuples: (int priority, SearchNode node) '''
-#     frontierQ = PriorityQueue()
-
-#     ''' Dictionary with:
-#         key = node ID
-#         value = {"searchNode": SearchNode, "explored": bool}
-#     '''
-#     frontierDict = {}
-
-#     startSearchNode = SearchNode(startNode, 0, startNode.getElevationGain(goalNode), None)
-#     frontierQ.put((startSearchNode.getPriority(), startSearchNode))
-
-#     #Initialize the currentNode
-#     currentSearchNode = None
-#     previousSearchNode = None
-    
-#     while not frontierQ.empty():
-#         #currentSearchNode is the searchNode with the smallest cost (smallest priority value)
-#         currentSearchNode = frontierQ.get()[1]
-
-#         #If the node has already been explored, skip this node:
-#         if frontierDict[currentSearchNode.getId()]["explored"]:
-#             continue
-
-#          #Add the currentSearchNode to the frontierDict as explored
-#         frontierDict[currentSearchNode.getId()] = {"searchNode": currentSearchNode, "explored": True}
-
-"""
-Each node is only considered explored once it is popped from the frontierQ
-Preventing nodes discovered on a path that exceeds maxPathLength from 
-"""
-
-""""""
+print(S.getNeighbors())
+print(A.getNeighbors())
 
 def minmax_elevation_gain(currentNode, goalNode, currentElevationGain, currentPathLength, maxPathLength, path, mode):
+    print("CEG", currentElevationGain)
     #If the current path length exceedes the maximum allowed path length, return an invalid path object
     if currentPathLength >= maxPathLength:
         return {"validPath": False}
@@ -84,7 +50,10 @@ def minmax_elevation_gain(currentNode, goalNode, currentElevationGain, currentPa
     if len(successors) == 0:
         return {"validPath": False}
 
+    #Initialize an invalid optimal path object to be worse than any valid path
     optimalPath = {"elevationGain": math.inf if mode == "minimize" else 0 , "validPath": False}
+    
+    #Set the optimal path to the path of the successor with the min or max elevation gain (depending on mode)
     for successor in successors:
         successorNode = successor["neighbor"]
         successorTotalElevationGain = currentElevationGain + successor["elevationGainToNeighbor"]
@@ -95,25 +64,5 @@ def minmax_elevation_gain(currentNode, goalNode, currentElevationGain, currentPa
             optimalPath = successorGoalPath
     print(optimalPath)
     return optimalPath
-        
 
-
-
-# def maximize_elevation_gain(startNode, goalNode, maxPathLength):
-#     if startNode.getId() == goalNode.getId():
-#         return goalNode
-
-print(minmax_elevation_gain(S, G, 0, 0, 45, ["S"], "maximize"))
-
-    
-
-# def optimize_elevation_gain(currentNode, goalNode, optimalPathLength, percentOfOptimal, maximize, currentdistance = 0, currentElevationGain = 0, path = []):
-#     #If the current node is the goal node, return elevationGain
-    
-#     #If the currentDistance of the path exceeds optimalPathLength*percentOfOptimal, return invalid path
-
-#     #If the currentElevationGain exceeds an elevation gain already seen, return invalid path
-
-
-
-#     return 
+print(minmax_elevation_gain(S, G, 0, 0, 450000000, ["S"], "minimize"))
