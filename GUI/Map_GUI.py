7d68fe6d50980dcfed5ad6c568e519cf755586a5
Emerson-Kiefer <emerson.kiefer01@gmail.com>
2022-12-08 01:04:34 -0500

    Rearanged GUI

--- GUI/Map_GUI.py
+++ GUI/Map_GUI.py
@@ -14,21 +14,44 @@ my_label = LabelFrame(root)
 # my_label.pack(pady=20)
 my_label.grid(row=0, column=2, rowspan=80)
 
-
-L1 = Label(root, text="Start Coordinates:")
+startLatitude = IntVar(None)
+L1 = Label(root, text="Start Latitude:")
 L1.grid(row=0, column=0)
-E1 = Entry(root, bd=1)
+E1 = Entry(root, bd=1, textvariable=startLatitude)
 E1.grid(row=0, column=1)
 
-L2 = Label(root, text="End Coordinates:")
+startLongitude = IntVar()
+L2 = Label(root, text="Start Longitude:")
 L2.grid(row=1, column=0)
-E2 = Entry(root, bd=1)
+E2 = Entry(root, bd=1, textvariable=startLongitude)
 E2.grid(row=1, column=1)
 
-L3 = Label(root, text="Percent of optimal distance :")
+endLatitude = IntVar()
+L3 = Label(root, text="End Latitude:")
 L3.grid(row=2, column=0)
-E3 = Entry(root, bd=1)
+E3 = Entry(root, bd=1, textvariable=endLatitude)
 E3.grid(row=2, column=1)
+
+endLongitude = IntVar()
+L4 = Label(root, text="End Longitude:")
+L4.grid(row=3, column=0)
+E4 = Entry(root, bd=1, textvariable=endLongitude)
+E4.grid(row=3, column=1)
+
+percent = IntVar()
+L5 = Label(root, text="Percent of optimal distance :")
+L5.grid(row=4, column=0)
+E5 = Entry(root, bd=1, textvariable=percent)
+E5.grid(row=4, column=1)
+
+
+def generatePath():
+    print("Percent: ", percent.get())
+    print("Start: ", startLatitude.get(), startLongitude.get())
+    print("End: ", endLatitude.get(), endLongitude.get())
+
+B1 = Button(root, text="Generate Path", fg="green", bg="black", command= generatePath)
+B1.grid(row=6, column=0)
 
 map_widget = tkintermapview.TkinterMapView(my_label, width=800, height=800, corner_radius=0)
 
@@ -37,7 +60,10 @@ map_widget.set_zoom(15)
 
 map_widget.add_right_click_menu_command(label="Add Marker", command=add_marker_event, pass_coords=True)
 
-marker_1 = map_widget.set_marker
+# path_1 = map_widget.set_path([marker_2.position, marker_3.position, (52.57, 13.4), (52.55, 13.35)])
+
+# marker_1 = map_widget.set_marker(startString.get())
+# if startString.get() != 
 map_widget.grid(row=0, column = 2, rowspan=10)
 
 root.mainloop()
