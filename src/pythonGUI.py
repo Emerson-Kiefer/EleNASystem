22c191b06d10b9ed5c07e308131eed16fd080449
Arnav Mohan <arnav0201@gmail.com>
2022-12-12 18:51:36 -0500

    PythonGUI

---
+++ src/pythonGUI.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QLineEdit
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore import QSize   
+import pyqtlet_map_widget
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(1000, 700))    
+        self.setWindowTitle("520 EleNA Project") 
+
+        self.nameLabel = QLabel(self)
+        self.nameLabel.setText('Name:')
+        self.line = QLineEdit(self)
+
+        self.line.move(80, 20)
+        self.line.resize(200, 32)
+        self.nameLabel.move(20, 20)
+
+        pybutton = QPushButton('OK', self)
+        pybutton.clicked.connect(self.clickMethod)
+        pybutton.resize(200,32)
+        pybutton.move(80, 60)        
+
+    def clickMethod(self):
+        print('Your name: ' + self.line.text())
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
